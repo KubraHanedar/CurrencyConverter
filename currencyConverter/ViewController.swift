085cbf58f2a971df5d6e50bcb3f9b47c4d1bf68e
KubraHanedar <the.hanedar@gmail.com>
2023-01-16 21:38:43 +0300

    tasarım olusturuldu

---
+++ currencyConverter/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  currencyConverter
+//
+//  Created by Kübra Hanedar on 16.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var chfLabel: UILabel!
+    @IBOutlet weak var gpbLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func getRatesClicked(_ sender: Any) {
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+}
+
