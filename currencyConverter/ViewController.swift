09bea9e0e31352038d9c19e2218913e6899edc23
KubraHanedar <the.hanedar@gmail.com>
2023-01-16 21:43:14 +0300

    request & session

--- currencyConverter/ViewController.swift
+++ currencyConverter/ViewController.swift
@@ -25,14 +25,36 @@ class ViewController: UIViewController {
     
     @IBAction func getRatesClicked(_ sender: Any) {
         
+        //  1 - REQUEST & SESSION
+        //  2 - RESPONSE & DATA
+        //  3 - PARSING & JSON SERIALIZATION
         
         
+        // 1.
+        let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/CurrencyData/main/currency.json")
         
+        let session = URLSession.shared
         
+        // CLOSURE
+        
+        let task = session.dataTask(with: url!) { (data, response, error) in
+            if error != nil {
+                let alert = UIAlertController(title: "error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true,completion: nil)
+            } else {
+                
+                // 2.
+                if data != nil {
+                   
+                   
+                }
+                
+            }
+        }
+        task.resume()
         
     }
-    
-    
-    
 }
 
